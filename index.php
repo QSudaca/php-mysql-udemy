0923217f45155ff93082f82a74d62574cabbf5b3
Eduardo Muñoz <97265526+QSudaca@users.noreply.github.com>
2023-01-10 14:41:11 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include('includes/header.php') ?>
+
+<body>
+    <h1>PHP CON INCLUDES</h1>
+    <a href="link.php">
+        <input type="button" value="Redirect">
+    </a>
+</body>
+</html>
---
+++ link.php
@@ -0,0 +1,5 @@
+<?php include('includes/header.php') ?>
+
+<a href="index.php">
+    <input type="button" value="back" class="btn btn-secondary">
+</a>
